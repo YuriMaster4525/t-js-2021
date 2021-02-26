924132c6ae6de9be116b304688d999fcbbe7ccfe
Devwarlt <devwarlt@loesoft.org>
2021-02-26 18:59:19 -0300

    First challenge solution using JavaScript and JQuery

--- src/js/external-js-scripts.js
+++ src/js/external-js-scripts.js
@@ -1,3 +1,11 @@
 function externalAlertFunction() {
     alert("Alert button clicked using external import!");
 }
+
+function replaceFnOutput(id, text) {
+    document.getElementById(id).innerText = text;
+}
+
+function replaceFnOutputJQuery(id, text) {
+    $("#" + id).text(text);
+}
